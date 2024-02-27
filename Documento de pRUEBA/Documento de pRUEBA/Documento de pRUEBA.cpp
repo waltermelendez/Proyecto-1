50890b2f5f7d2c588edccdbcfc9e952e791ad61c
Walter Meléndez <100178332+waltermelendez@users.noreply.github.com>
2024-02-27 11:05:18 -0600

    Mi cambio
    mi espacii

--- Documento de pRUEBA/Documento de pRUEBA/Documento de pRUEBA.cpp
+++ Documento de pRUEBA/Documento de pRUEBA/Documento de pRUEBA.cpp
@@ -7,6 +7,7 @@ int main()
     std::cout << "Hola!\n";
     std::cout << "xd!\n";
     cout << "docuemnto de pruba\n";
+    cout << "espacii\n";
     int r = 45;
     int q = 44;
     cout << r + q << endl;
