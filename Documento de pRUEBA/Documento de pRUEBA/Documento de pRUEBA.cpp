d1f52d0c66ad0ec6a13c9ebbf14240e0f69f7045
Walter Meléndez <100178332+waltermelendez@users.noreply.github.com>
2024-02-27 10:23:08 -0600

    Prueba
    Prueba de modificacion.

---
+++ Documento de pRUEBA/Documento de pRUEBA/Documento de pRUEBA.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << "docuemnto de pruba\n";
+    int r = 45;
+    int q = 44;
+    cout << r + q << endl;
+}
